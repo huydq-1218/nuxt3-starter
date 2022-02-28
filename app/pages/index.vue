7488ae3e45f77c1f0817c69fa5cf4be7893cc4bb
Truong Hoang Phuong <thphuong1093@gmail.com>
2022-02-28 15:46:31 +0700

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        {{ auth.user }}
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { useAuthStore } from '~/store';
+
+    const auth = useAuthStore();
+</script>
